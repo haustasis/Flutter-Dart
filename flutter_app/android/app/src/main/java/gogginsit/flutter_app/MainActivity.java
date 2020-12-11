fd84be2e015d6a8045efcbade7a1ff87ba9c53ac
TARISO <toni.ariso@gmail.com>
2020-12-11 10:38:15 +0100

    new begin

---
+++ flutter_app/android/app/src/main/java/gogginsit/flutter_app/MainActivity.java
@@ -0,0 +1,6 @@
+package gogginsit.flutter_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ i_am_rich/android/app/src/main/java/dev/haustasis/i_am_rich/MainActivity.java
@@ -0,0 +1,6 @@
+package dev.haustasis.i_am_rich;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
