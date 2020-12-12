34dcad4b5fa5eb38a50018cba743e2d1a40c429e
TARISO <toni.ariso@gmail.com>
2020-12-12 04:10:35 +0100

    container

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
       ),
     ),
   );
